67467dafc694f312f7444c848fea514186bc3618
vaskania26 <vnikabadze@unisens.ge>
2021-11-16 15:28:34 +0400

    added routing web pages

--- server.js
+++ server.js
@@ -4,15 +4,61 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
+  // Set header content-type
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", (err, data) => {
+
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  // Send a html file
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.error(err);
-      res.end();
     } else {
       res.end(data);
     }
   });
+
+  // Send a html file
+  /*  if (req.url === "/" || req.url === "/home") {
+    fs.readFile("./views/index.html", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      } else {
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("./views/about.html", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      } else {
+        res.end(data);
+      }
+    });
+  } else {
+    fs.readFile("./views/404.html", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      } else {
+        res.end(data);
+      }
+    });
+  } */
 });
 
 server.listen(3000, "localhost", () => {
