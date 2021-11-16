892ab4cc511210e66ba9de135662a7ce0b9e2842
vaskania26 <vnikabadze@unisens.ge>
2021-11-16 14:13:05 +0400

    added createWriteStream

--- streams.js
+++ streams.js
@@ -1,7 +1,17 @@
 const fs = require("fs");
 
-const readStream = fs.createReadStream("./docs/blog2.txt", "utf8");
+/* const readStream = fs.createReadStream("./docs/blog2.txt", "utf8");
 readStream.on("data", (chunk) => {
   console.log("---------- NEW CHUNK ----------");
   console.log(chunk);
+}); */
+
+const readStream = fs.createReadStream("./docs/blog2.txt", "utf8");
+const writeStream = fs.createWriteStream("./docs/blog3.txt");
+
+readStream.on("data", (chunk) => {
+  console.log("---------- NEW CHUNK ----------");
+  console.log(chunk);
+  writeStream.write("\nNEW CHUNK\n");
+  writeStream.write(chunk);
 });
