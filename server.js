d96a0fb2387cb05d313070c7f987af373dd71614
vaskania26 <vnikabadze@unisens.ge>
2021-11-16 15:04:38 +0400

    added content-type text/plain

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello vaska");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
